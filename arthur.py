f5ae47f15461689e8d4e743a994dd61203815222
Arthur <arthuremanueln.lima@gmail.com>
2025-07-01 11:26:38 -0300

    Add files via upload

--- arthur.py
+++ arthur.py
@@ -1,6 +1,14 @@
 numero = int(input("Digite um número: "))
+opção = input("Digite 's' para repetir a ação ou qualquer outra tecla para sair: ")
 
-if numero % 2 == 0:
+if  numero % 2 == 0:
     print(f"{numero} é par")
-else:
+elif numero % 2 == 1:
     print(f"{numero} é ímpar")
+while opção == 's':
+    numero = int(input("Digite um número: "))
+    if numero % 2 == 0:
+        print(f"{numero} é par")
+    elif numero % 2 == 1:
+        print(f"{numero} é ímpar")
+    opção = input("Digite 's' para repetir a ação ou qualquer outra tecla para sair: ")
